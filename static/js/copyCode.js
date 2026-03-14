5d461ab1bfeef8df147b710db70383e6f9ffe819
Adam Green <adam.green@adgefficiency.com>
2026-03-14 18:03:26 +1300

    feat

--- static/js/copyCode.js
+++ static/js/copyCode.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const pre = codeBlock.parentElement
     const figure = pre.parentElement
 
-    if (figure.classList.contains('output-block')) return
-
     // Create copy button
     const copyButton = document.createElement('button')
     copyButton.className = 'copy-code-btn'
